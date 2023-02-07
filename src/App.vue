547db825d4126d7f9ee1e490642d84c08ae71a3c
эрвин бекиров <tom.margan@mail.ru>
2023-02-07 16:22:50 +0300

    3 упражнение

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+	export default {
+		name: 'App',
+	}
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+		Hello World
+	</div>
 </template>
 
 <style scoped>
@@ -28,12 +20,11 @@ header {
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
 
+  div
+  {
+    color:red
+  }
   .logo {
     margin: 0 2rem 0 0;
   }
