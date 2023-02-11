df57078a125ed159c976a033d460fba8ef89884f
эрвин бекиров <tom.margan@mail.ru>
2023-02-11 13:56:32 +0300

    42 упражнение

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,14 @@
 
 </script>
 <template >
- <p :class="{active: isValid, error: isDisabled}">Yunus Mustafev</p>
- <p :class="obj">2</p>
+ <p style="{font-weight: bold;}">Yunus Mustafev</p>
+ <p>2</p>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
+  
 }
 
 .logo {
@@ -32,13 +33,17 @@ header {
 
   p {
     color: red;
-    display: block;
+    border: '1px solid green'
+
+    
 }
   .logo {
     margin: 0 2rem 0 0;
+    
   }
 
   header .wrapper {
+    
     display: flex;
     place-items: flex-start;
     flex-wrap: wrap;
