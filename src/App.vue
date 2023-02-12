03443e96d0ef2114579e07158398676d911cfef7
эрвин бекиров <tom.margan@mail.ru>
2023-02-12 14:11:30 +0300

    55 упражнение

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,33 @@
 	export default {
 		data() {
       return {
-       
-        items: ['a', 'b', 'c', 'd', 'e'],
+        users: [
+			{
+				id: 1,
+				name: 'Yunus Mustafaev',
+				salary: 100,
+				age: 30,
+			},
+			{
+				id: 2,
+				name: 'name2',
+				salary: 200,
+				age: 40,
+			},
+			{
+				id: 3,
+				name: 'name3',
+				salary: 300,
+				age: 50,
+			},
+		],
 	} 
 },
 methods: {
-	removeItem: function(index) {
-		this.items.splice(index, 1);
+	removeItem: function(id) {
+		this.users = this.users.filter((user) => {
+			return user.id !== id;
+		})
 	}
 }
 
@@ -16,9 +36,14 @@ methods: {
 </script>
 <template >
   <ul>
-		<li v-for="(item, index) in items" :key="index">
-			{{ item }}
-			<button @click="removeItem(index)">remove</button>
+		<li v-for="user in users" :key="user.id">
+			{{ user.name }}
+			{{ user.surn }}
+      <p>Salary</p>
+      {{ user.salary }}
+      <p>Age:</p>
+      {{ user.age }}
+			<button @click="removeItem(user.id)">remove</button>
 		</li>
 	</ul>
 	
