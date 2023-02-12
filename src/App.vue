a2ce3cee8ee5b12b7a7a9420a0b359d046a670fe
эрвин бекиров <tom.margan@mail.ru>
2023-02-12 14:14:43 +0300

    57 упражнение

--- src/App.vue
+++ src/App.vue
@@ -2,58 +2,20 @@
 	export default {
 		data() {
       return {
-        users: [
-			{
-				id: 1,
-				name: 'name1',
-				surn: 'surn1',
-				isEdit: false,
-			},
-			{
-				id: 2,
-				name: 'name2',
-				surn: 'surn2',
-				isEdit: false,
-			},
-			{
-				id: 3,
-				name: 'name3',
-				surn: 'surn3',
-				isEdit: false,
-			},
-		]
+        
 	} 
 },
-methods: {
-	edit(user) {
-		user.isEdit = true;
-	},
-	save(user) {
-		user.isEdit = false;
-	},
-}
-
   }
 </script>
 <template >
- <ul>
-		<li v-for="user in users" :key="user.id">
-			<template v-if="!user.isEdit">
-				{{ user.name }}
-				{{ user.surn }}
-				<button @click="edit(user)">
-					edit
-				</button>
-			</template>
-			<template v-else>
-				<input v-model="user.name">
-				<input v-model="user.surn">
-				<button @click="save(user)">
-					save
-				</button>
-			</template>
-		</li>
-	</ul>
+		<p>Работа с компонентами в Vue
+Если мы посмотрим на какой-нибудь сайт, то можем выделить на нем некоторые блоки: хедер, контент, сайдбар, футер.
+
+При создании сайта на Vue мы также можем использовать такие независимые блоки. Они позволят нам лучше организовать наш код и упростят его дальнейшую поддержку.
+
+Во Vue такие блоки называются компонентами. В крупных приложениях разделение на компоненты становится обязательным условием для сохранения управляемости процесса разработки
+
+Компоненты рекомендовано размещать в папке components. Каждый компонент в отдельном файле в виде модуля ES6.</p>
 	
 </template>
 
