8c28111ba4d479ee5bcac8d8ff8a1dc04dacf196
эрвин бекиров <tom.margan@mail.ru>
2023-02-12 16:31:06 +0300

    63 упражнение

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <script>
 import User from './components/User.vue'
 	export default {
-    props: {
-		name: String,
-		surn: String
-	},
 		components: {
 			User
+		},
+    data() {
+			return {
+				users: [
+			{
+				id: 1,
+				name: 'name1',
+				salary: 100,
+				age: 30,
+			},
+			{
+				id: 2,
+				name: 'name2',
+				salary: 200,
+				age: 40,
+			},
+			{
+				id: 3,
+				name: 'name3',
+				salary: 300,
+				age: 50,
+			},
+		],
+			}
 		}
 }
 </script>
 <template >
-	<User :name="name" :surn="surn" />
+	<User
+		v-for="user in users"
+    :id="user.id"
+		:name="user.name"
+		:salary="user.salary"
+		:age="user.age"
+	/>
 </template>
 
 <style scoped>
--- src/components/User.vue
+++ src/components/User.vue
@@ -1,19 +1,36 @@
 <script>
 	export default {
-		props: {
-		name: String,
-		surn: String
-	},
+	
 		data() {
 			return {
-				name: 'Yunus',
-		surn: 'Mustafaev',
+				users: [
+			{
+				id: 1,
+				name: 'name1',
+				salary: 100,
+				age: 30,
+			},
+			{
+				id: 2,
+				name: 'name2',
+				salary: 200,
+				age: 40,
+			},
+			{
+				id: 3,
+				name: 'name3',
+				salary: 300,
+				age: 50,
+			},
+		],
 			}
 		}
 	}
 </script>
 <template>
+	{{ id  }}
 	{{ name }}
-	{{ surn }}
+	{{ salary }}
+	{{ age }}
 	
 </template>
