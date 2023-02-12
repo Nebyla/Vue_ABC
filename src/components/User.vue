a89a0cc338a778421998c48b3191e3d3a62b39b6
эрвин бекиров <tom.margan@mail.ru>
2023-02-12 16:38:13 +0300

    65 упражнение

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,36 +1,19 @@
 <script>
 	export default {
-	
-		data() {
-			return {
-				users: [
-			{
-				id: 1,
-				name: 'name1',
-				salary: 100,
-				age: 30,
-			},
-			{
-				id: 2,
-				name: 'name2',
-				salary: 200,
-				age: 40,
-			},
-			{
-				id: 3,
-				name: 'name3',
-				salary: 300,
-				age: 50,
-			},
-		],
-			}
+	emits: ['show'],
+	data() {
+		return {
+			
 		}
+	},
+	methods: {
+	handle() {
+		this.$emit('show');
 	}
+}
+}
 </script>
 <template>
-	{{ id  }}
-	{{ name }}
-	{{ salary }}
-	{{ age }}
 	
+	<button @click="handle">btn</button>
 </template>
